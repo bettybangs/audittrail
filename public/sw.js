1f607a91f4bc05670c4505f69e0d8761c2ad4418
Beth Gerharts <bettybangs@gmail.com>
2026-06-16 20:37:04 -0400

    add PWA support

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+const CACHE_NAME = 'risk-whisperer-v1';
+const urlsToCache = ['/', '/index.html', '/static/js/main.chunk.js', '/static/css/main.chunk.css'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache).catch(() => {})));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request).catch(() => {}))
+  );
+});
